50f2e4c3b85867da49c6c40817ac2338ee28ebce
Crcker <80049662+Crcker@users.noreply.github.com>
2022-10-08 20:38:25 +0300

    Add files via upload

---
+++ lw1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace lw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "Неизвестная_ошибка";
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Неизвестная_ошибка");
+                Environment.Exit(0);
+            }
+            bool aIsNumeric = double.TryParse(args[0], out double a);
+            bool bIsNumeric = double.TryParse(args[1], out double b);
+            bool cIsNumeric = double.TryParse(args[2], out double c);
+            if ((aIsNumeric == false) ^ (bIsNumeric == false) ^ (cIsNumeric == false))
+            {
+                Console.WriteLine("Неизвестная_ошибка");
+                Environment.Exit(0);
+            }
+            if ((a <= 0) ^ (b <= 0) ^ (c <= 0))
+            {
+                Console.WriteLine("Неизвестная_ошибка");
+                Environment.Exit(0);
+            }
+            if ((a + b > c) & (a + c > b) & (b + c > a))
+            {
+                ans = "Обычный";
+            }
+            else
+            {
+                Console.WriteLine("Не_треугольник");
+                Environment.Exit(0);
+            }
+            if ((a == b) ^ (a == c) ^ (b == c))
+            {
+                ans = "Равнобедренный";
+            }
+
+            if ((a == b) & (a == c))
+            {
+                ans = "Равносторонний";
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ lw1tests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace lw1tests
+{
+    class Testing
+    {
+        static void Main()
+        {
+            string filePath = "D:\\TiOPO\\lw1\\bin\\Debug\\netcoreapp3.1\\lw1.exe";
+            string testPath = "D:\\TiOPO\\lw1tests\\tests\\test.txt";
+            string outPath = "D:\\TiOPO\\lw1tests\\tests\\testout.txt";
+
+            using StreamReader sr = new(testPath);
+            using StreamWriter sw = new(outPath);
+            
+            string inArgs;
+
+            while ((inArgs = sr.ReadLine()) != null)
+            {
+            
+                inArgs = inArgs.Replace('\t', ' ');
+                var testArgs = inArgs.Split(' ');
+                if (testArgs.Length == 4)
+                {
+                    string a = testArgs[0];
+                    string b = testArgs[1];
+                    string c = testArgs[2];
+                    string result = testArgs[3];
+
+                    using Process prgram = new();
+                    prgram.StartInfo.FileName = filePath;
+                    prgram.StartInfo.Arguments = $"{testArgs[0]} {testArgs[1]} {testArgs[2]}";
+                    prgram.StartInfo.UseShellExecute = false;
+                    prgram.StartInfo.RedirectStandardOutput = true;
+                    prgram.Start();
+
+                    StreamReader reader = prgram.StandardOutput;
+                    string programResult = reader.ReadLine();
+                    string outRes = (programResult == result) ? "success" : "error";
+                    sw.WriteLine(outRes);
+                }
+                else
+                {
+                    sw.WriteLine("error");
+                }              
+            }
+        }
+    }
+}
